046990307de847f18f9aa73cea9ba2c4f8865e82
Anant Gupta <anant.gupta@ekahal.com>
2024-01-07 02:59:39 +0530

    card min width change

--- src/assets/wrappers/CocktailList.js
+++ src/assets/wrappers/CocktailList.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
   gap: 2rem;
 `;
 
